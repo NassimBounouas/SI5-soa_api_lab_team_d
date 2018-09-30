dbf799ea6007918165a46f228a501a46ee016875
DUMINY Gaétan <33255793+Viperlion@users.noreply.github.com>
2018-09-30 15:54:42 +0200

    POST Only

--- Eta.py
+++ Eta.py
@@ -1,43 +1,39 @@
 from flask import Flask, request, jsonify
 import random
 app = Flask(__name__)
-jsonFile = {}
-jsonExist = False
 
 __product__ = "Eta Service"
 __author__ = "Duminy Gaetan"
 __copyright__ = "Copyright 2018, Polytech Nice Sophia"
 __credits__ = ["Duminy Gaetan"]
 __license__ = "MIT"
-__version__ = "1.0"
+__version__ = "1.0.1"
 __maintainer__ = "Duminy Gaetan"
 __email__ = "gaetan.duminy@etu.unice.fr"
 __status__ = "development"
 
 
-@app.route('/receiveOrder', methods=['POST'])
-def receiveOrder():
+@app.route('/receive_event', methods=['POST'])
+def computeEta():
     if request.is_json :
-        global jsonFile, jsonExist
         jsonFile = request.get_json()
-        jsonExist = True
-        return 'JSON receive', 200
+        if jsonFile["Action"] == "compute_eta":
+            message = jsonFile["Message"]
+            time1 = random.randint(10, 20)
+            time2 = time1 + random.randint(5, 15)
+            data = { "Restaurant": message['Restaurant'],
+                     "Meal": message['Meal'],
+                     "DeliveryAddress": message['DeliveryAddress'],
+                     "PickUpDate": time1,
+                     "DeliveryDate": time2
+                }
+            return jsonify( Action = "validate_order",
+                            Message = data), 200
+                                
+        else :
+            return "Wrong service, the action here is compute_eta"
     else :
         return 'Not a JSON', 222
-    
-@app.route('/computeEta', methods=['GET'])
-def computeEta():
-    global jsonFile, jsonExist
-    if jsonExist :
-        time1 = random.randint(10, 20)
-        time2 = time1 + random.randint(5, 15)
-        return jsonify( Restaurant = jsonFile['Restaurant'],
-                        Meal = jsonFile['Meal'],
-                        DeliveryAddress = jsonFile['DeliveryAddress'],
-                        PickUpDate = time1,
-                        DeliveryDate = time2), 200
-    else :
-        return 'Send a JSON first', 200
 
 @app.route('/')
 def main():
