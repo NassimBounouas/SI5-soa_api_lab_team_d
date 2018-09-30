d5333754417bdd9ad7de487113c57fcc559fec44
MEERSMAN Rudy <33250028+Rmeersman@users.noreply.github.com>
2018-09-30 15:57:42 +0200

    OrderingService02.5

--- ServiceCommand.py
+++ ServiceCommand.py
@@ -5,45 +5,51 @@ import random
 app = Flask(__name__)
 
 
-@app.route('/OrderMeal/<string:meal>',methods = ['GET'])
-def orderMeal(meal : str):
+def orderMeal(jsonRecv):
     resto = ""
     price = ""
     with open("Restaurant.json") as f:
         file = json.load(f)
     f.close()
     for i in file["plat"]:
-        if i["Name"] == meal:
-            resto = i["Restaurant"]
-            price = i["Price"]
+        if i["name"] == jsonRecv["meal"]:
+            resto = i["restaurant"]
+            price = i["price"]
             break
-    return jsonify( Order = {'Restaurant' : resto,
-                    'Meal' : meal,
-                    'Price' : price
+    data = {
+        'restaurant' : resto,
+        'meal' : jsonRecv["meal"],
+        'price' : price
     }
+    return jsonify(
+        action = "compute_eta",
+        message = data
     )
 
-@app.route('/ValidateOrder/<string:acceptation>',methods = ['POST'])
-def validateOrder(acceptation : str):
-    jsonMessage = request.get_json(force=True)
-    if acceptation == "OK":
-        data =  {
-            'Command_Id' : random.randint(0,100),
-            'Restaurant' : jsonMessage["Restaurant"],
-            'Meal' : jsonMessage["Meal"],
-            'Delivery_Address' : jsonMessage["Delivery_Address"],
-            'Pick_Up_Date' : jsonMessage["Pick_Up_Date"],
-            'Delivery_Date' : jsonMessage["Delivery_Date"],
-            'Total Price' : jsonMessage["Total Price"]
+def validateOrder(jsonMessage):
+    data =  {
+            'command_id' : random.randint(0,100),
+            'restaurant' : jsonMessage["restaurant"],
+            'meal' : jsonMessage["meal"],
+            'delivery_address' : jsonMessage["delivery_address"],
+            'delivery_date' : jsonMessage["delivery_date"],
+            'price' : jsonMessage["price"]
     }
+    return jsonify(
+        action = data,
+        status = "Accepted")
+
+@app.route('/receive_event',methods = ['POST'])
+def main():
+    jsonMessage = request.get_json(force=True)
+    if jsonMessage["action"] == "validate_order":
+        return validateOrder(jsonMessage["message"])
+    elif jsonMessage["action"] == "order_meal":
+        return orderMeal(jsonMessage["message"])
+    else:
         return jsonify(
-                    Order = data,
-                    Status = "Accepted")
-    else :
-        return jsonify(
-            Order = "none",
-            Status = "Canceled"
+            error = "404 Not Found"
         )
 
 if __name__ == '__main__':
-    app.run
+    app.run()
