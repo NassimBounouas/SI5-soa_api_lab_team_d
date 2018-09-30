f74e5d9499d5e3dcfa9117803f9688b600bfec99
Nassim Bounouas <nassim.bounouas@etu.unice.fr>
2018-09-30 16:39:57 +0200

    Menu service + Use Case1 update

--- UseCase1.sh
+++ UseCase1.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
+echo -e " \n\n### REQUEST MENU TO MENU SERVICE\n"
+curl -s -X POST --header "Content-Type: application/json" http://localhost:4000/receive_event --data '{
+	"Action": "READ_CATEGORIES",
+	"Message": {}
+}' | json_pp
+
+echo -e " \n\n### REQUEST MENU WITH JAPANASE CATEGORY\n"
+curl -s -X POST --header "Content-Type: application/json" http://localhost:4000/receive_event --data '{
+	"Action": "READ_MEALS_BY_CATEGORY",
+	"Message": {
+		"Category": "Japonais"
+	}
+}' | json_pp
+
+
 echo -e " \n\n### SENDING ORDER TO RESTAURANT SERVICE\n"
-curl -X POST -i http://localhost:4003/receive_event --data '{
+curl -X POST http://localhost:4003/receive_event --data '{
 "Action":"Receive_order",
 "Message": "{\"Meal\":\"Ramen\",\"RestaurantAdress\":\"Lyangsrestaurant\",\"DeliveryAdress\":\"Polytech Nice Sophia\"}"
 }'
 
 echo -e "\n\n### SENDING DELIVERY REQUEST TO DELIVERY SERVICE\n"
-curl -X POST -i http://localhost:4004/receive_event --data '{
+curl -X POST http://localhost:4004/receive_event --data '{
 "Action":"Delivery_request",
 "Message": "{\"Meal\":\"Ramen\",\"RestaurantAdress\":\"Lyangsrestaurant\",\"DeliveryAdress\":\"Polytech Nice Sophia\"}"
 }'
+
+echo -e "\n\n"
