a2df176af590615a46b983a80f4ea985dba0e28e
MEERSMAN Rudy <33250028+Rmeersman@users.noreply.github.com>
2018-09-30 13:40:25 +0200

    OrderingService02.2
    Update Order Service and change the return Json

--- ServiceCommand.py
+++ ServiceCommand.py
@@ -8,26 +8,42 @@ app = Flask(__name__)
 @app.route('/OrderMeal/<string:meal>',methods = ['GET'])
 def orderMeal(meal : str):
     resto = ""
+    price = ""
     with open("Restaurant.json") as f:
         file = json.load(f)
     f.close()
     for i in file["plat"]:
         if i["Name"] == meal:
             resto = i["Restaurant"]
+            price = i["Price"]
             break
-    return jsonify( Restaurant = resto,
-                    Meal = meal))
+    return jsonify( Order = {'Restaurant' : resto,
+                    'Meal' : meal,
+                    'Price' : price
+    }
+    )
 
-@app.route('/ValidateOrder',methods = ['POST'])
-def validateOrder():
+@app.route('/ValidateOrder/<string:acceptation>',methods = ['POST'])
+def validateOrder(acceptation : str):
     jsonMessage = request.get_json(force=True)
-    return jsonify(
-                   Restaurant = jsonMessage["Restaurant"],
-                   Meal = jsonMessage["Meal"],
-                   Delivery_Address = jsonMessage["Delivery_Address"],
-                   Pick_Up_Date = jsonMessage["Pick_Up_Date"],
-                   Delivery_Date = jsonMessage["Delivery_Date"],
-                   Command_Id = random.randint(0,100))
+    if acceptation == "OK":
+        data =  {
+            'Command_Id' : random.randint(0,100),
+            'Restaurant' : jsonMessage["Restaurant"],
+            'Meal' : jsonMessage["Meal"],
+            'Delivery_Address' : jsonMessage["Delivery_Address"],
+            'Pick_Up_Date' : jsonMessage["Pick_Up_Date"],
+            'Delivery_Date' : jsonMessage["Delivery_Date"],
+            'Total Price' : jsonMessage["Total Price"]
+    }
+        return jsonify(
+                    Order = data,
+                    Status = "Accepted")
+    else :
+        return jsonify(
+            Order = "none",
+            Status = "Canceled"
+        )
 
 if __name__ == '__main__':
-    app.run()
+    app.run
