b3f8e4875ed28d888d558d96a86b49b5141b4d86
Nassim Bounouas <nassim.bounouas@etu.unice.fr>
2018-10-02 00:21:26 +0200

    schema updated

--- schema.sql
+++ schema.sql
@@ -1,8 +1,11 @@
-CREATE DATABASE test_db; 
-USE test_db;
-CREATE TABLE test_table (
+CREATE DATABASE delivery_db; 
+USE delivery_db;
+
+CREATE TABLE to_deliver_table (
 `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-test_name varchar(255) 
+meal_name varchar(255),
+pickup_address varchar(255),
+delivery_address varchar(255)
 );
 
-INSERT INTO test_table (test_name) VALUES ("test_entry");
+INSERT INTO to_deliver_table (meal_name, pickup_address, delivery_address) VALUES ("test_meal", "test_pickup", "test_delivery");
