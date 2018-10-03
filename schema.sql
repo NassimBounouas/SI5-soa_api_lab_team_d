e7dfde88b72fde0e7e21a6b468b9a0a532cc71b2
MEERSMAN Rudy <33250028+Rmeersman@users.noreply.github.com>
2018-10-03 18:38:14 +0200

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,8 +1,18 @@
-CREATE DATABASE test_db; 
-USE test_db;
-CREATE TABLE test_table (
+CREATE DATABASE ordering_db; 
+USE ordering_db;
+
+CREATE TABLE to_get_restaurant (
 `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-test_name varchar(255) 
+meal_name varchar(255)
 );
 
-INSERT INTO test_table (test_name) VALUES ("test_entry");
+CREATE TABLE to_get_recipe(
+`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+meal_name varchar(255),
+restaurant_name varchar(255),
+pickup_address varchar(255),
+delivery_adress varchar(255)
+);
+
+INSERT INTO to_get_restaurant (meal_name) VALUES ("test_meal");
+INSERT INTO to_get_recipe (meal_name,restaurant_name,pickup_address,delivery_address) VALUES ("test_meal",test_restaurant","test_pickup","test_delivery");
