83ff9c8282ab6f6023d1404ea832ab04dfdbd0c1
Nassim Bounouas <nassim.bounouas@etu.unice.fr>
2018-10-06 22:18:45 +0200

    Return an Event

--- handlers.go
+++ handlers.go
@@ -16,24 +16,41 @@ func ReceiveEvents(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(err)
 		return
 	}
-	//w.Header().Set("Content-Type", "plain/text")
-	w.Write([]byte(ProcessEvent(event)))
+	event = ProcessEvent(event)
+	marshalledEvent, err := json.Marshal(event)
+	if err != nil {
+		http.Error(w,err.Error(), http.StatusInternalServerError)
+		fmt.Println(err)
+		return
+	}
+	w.Write([]byte(marshalledEvent))
 }
 
-func ProcessEvent(event Event) string {
+func ProcessEvent(event Event) Event {
 	if event.Action == "Delivery_request" {
 		var order Order
 		err := json.Unmarshal([]byte(event.Message), &order)
 		if err != nil {
 			fmt.Println(err)
 			fmt.Println("Error while unmarshalling order")
-			return ""
+			var errorEvent Event
+			errorEvent.Action = "Error"
+			errorEvent.Message = "The submitted Order is malformed"
+			return errorEvent
 		}
 		Add_to_deliver(order.Meal, order.RestaurantAdress, order.DeliveryAdress)
-		return "Your request has been accepted, your " + order.Meal + " will be picked up at " + order.RestaurantAdress + " and delivered to " + order.DeliveryAdress
+		var returnedEvent Event
+		returnedEvent.Action = "Response"
+		returnedEvent.Message = "Your request has been accepted, your " + order.Meal + " will be picked up at " + order.RestaurantAdress + " and delivered to " + order.DeliveryAdress
+		return returnedEvent
 	} else if event.Action == "List_request" {
-		fmt.Println(strings.Join(Read_to_delivers(), ","))
-			return strings.Join(Read_to_delivers(), ",")
+		var returnedEvent Event
+		returnedEvent.Action = "Response"
+		returnedEvent.Message = strings.Join(Read_to_delivers(), ", ")
+		return returnedEvent
 	}
-	return "unknown action"
+	var errorEvent Event
+	errorEvent.Action = "Error"
+	errorEvent.Message = "The submitted action is unknown"
+	return errorEvent
 }
