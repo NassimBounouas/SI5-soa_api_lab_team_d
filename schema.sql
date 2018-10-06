e5c8b5adcf3b98e7be485fa88faa7a71e757090f
Nassim Bounouas <nassim.bounouas@etu.unice.fr>
2018-10-07 01:02:07 +0200

    Schema updated

--- schema.sql
+++ schema.sql
@@ -1,8 +1,12 @@
-CREATE DATABASE test_db; 
-USE test_db;
-CREATE TABLE test_table (
+CREATE DATABASE chinese_restaurant_db; 
+USE chinese_restaurant_db;
+
+CREATE TABLE to_prepare_table (
 `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-test_name varchar(255) 
+order_time DATETIME,
+pickup_date DATETIME,
+client varchar(255),
+meal_name varchar(255)
 );
 
-INSERT INTO test_table (test_name) VALUES ("test_entry");
+-- INSERT INTO to_prepare_table (order_time, meal_name) VALUES ("2018-10-06T23:58:29+02:00", "test_meal");
