0a586eb4f8244709882c099474d5fcebe4766575
Nassim Bounouas <nassim.bounouas@etu.unice.fr>
2018-10-07 15:11:45 +0200

    Schema updated

--- schema.sql
+++ schema.sql
@@ -5,7 +5,7 @@ CREATE TABLE to_deliver_table (
 `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 meal_name varchar(255),
 pickup_address varchar(255),
+pickup_date DATETIME,
+client varchar(255),
 delivery_address varchar(255)
 );
-
-INSERT INTO to_deliver_table (meal_name, pickup_address, delivery_address) VALUES ("test_meal", "test_pickup", "test_delivery");
