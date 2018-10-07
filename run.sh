a9d93c996fad328cba04a613d27170aa870ec908
Nassim Bounouas <nassim.bounouas@etu.unice.fr>
2018-10-07 18:06:42 +0200

    Wait Databases in run.sh

--- run.sh
+++ run.sh
@@ -7,11 +7,28 @@ BLUE='\033[0;34m'
 YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
+####  MANUAL WAIT FOR DATABASES WARM UP
+
+echo -e  "Waiting all databases\n"
+echo "1/5";
+sleep 5;
+echo "2/5";
+sleep 5;
+echo "3/5";
+sleep 5;
+echo "4/5";
+sleep 5;
+echo "5/5";
+sleep 2;
+
+####
 echo -e " \n\n${RED}### REQUEST MENU TO MENU SERVICE${NC}\n"
 curl -s -X POST --header "Content-Type: application/json" -i http://localhost:4000/receive_event --data '{
 	"Action": "READ_CATEGORIES",
 	"Message": {}
 }'
+
+sleep 4;
 
 echo -e " \n\n${RED}### REQUEST MENU WITH JAPANASE CATEGORY${NC}\n"
 curl -s -X POST --header "Content-Type: application/json" -i http://localhost:4000/receive_event --data '{
@@ -20,6 +37,8 @@ curl -s -X POST --header "Content-Type: application/json" -i http://localhost:40
 		"Category": "Japonais"
 	}
 }'
+
+sleep 4;
 
 echo -e " \n\n${GREEN}### ORDER A RAMEN MEAL${NC}\n"
 curl -s -X POST -i http://localhost:4001/receive_event --data '{
@@ -30,6 +49,8 @@ curl -s -X POST -i http://localhost:4001/receive_event --data '{
     }
 }'
 
+sleep 4;
+
 echo -e " \n\n${ORANGE}### COMPUTE ETA FOR THE ORDER${NC}\n"
 curl -X POST -H 'Content-Type: application/json' -i http://localhost:4002/receive_event --data '{	
 	"Action": "compute_eta",
@@ -39,6 +60,8 @@ curl -X POST -H 'Content-Type: application/json' -i http://localhost:4002/receiv
 	"Delivery_Address": "Polytech Nice Sophia"
 	}
 }'
+
+sleep 4;
 
 echo -e " \n\n${GREEN}### VALIDATE THE RAMEN MEAL ORDER${NC}\n"
 curl -s -X POST -i http://localhost:4001/receive_event --data '{
@@ -53,11 +76,15 @@ curl -s -X POST -i http://localhost:4001/receive_event --data '{
     }
 }'
 
+sleep 4;
+
 echo -e " \n\n${BLUE}### SENDING ORDER TO RESTAURANT SERVICE${NC}\n"
 curl -s -X POST -i http://localhost:4003/receive_event --data '{
 "Action":"Receive_order",
 "Message": "{\"Meal\":\"Ramen\",\"Client\":\"Philippe C.\",\"PickUpDate\":\"2018-10-10T12:00:00+02:00\"}"
 }'
+
+sleep 4;
 
 echo -e "\n\n${YELLOW}### SENDING DELIVERY REQUEST TO DELIVERY SERVICE${NC}\n"
 curl -s -X POST -i http://localhost:4004/receive_event --data '{
