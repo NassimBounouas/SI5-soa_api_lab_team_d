2bdd2831eb3b0c4fb1b847eaac04387723da7254
MEERSMAN Rudy <33250028+Rmeersman@users.noreply.github.com>
2018-10-07 12:01:08 +0200

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -14,3 +14,6 @@ restaurant_name varchar(255),
 delivery_date integer,
 delivery_adress varchar(255)
 );
+
+ INSERT INTO to_get_restaurant(meal_name,restaurant_name) VALUES ("Ramen","Lyianhg Restaurant");
+ INSERT INTO to_get_restaurant(meal_name,restaurant_name) VALUES ("Pizza","Bar Roger");
