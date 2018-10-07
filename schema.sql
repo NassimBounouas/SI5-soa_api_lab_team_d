960229db103e517c2823ddb1db1652ca39235595
MEERSMAN Rudy <33250028+Rmeersman@users.noreply.github.com>
2018-10-07 12:46:30 +0200

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -4,14 +4,15 @@ USE ordering_db;
 CREATE TABLE to_get_restaurant (
 `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 meal_name varchar(255),
-restaurant_name varchar(255)
+restaurant_name varchar(255),
+price integer
 );
 
 CREATE TABLE to_get_recipe(
 `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 meal_name varchar(255),
 restaurant_name varchar(255),
-delivery_date integer,
+delivery_date DATETIME,
 delivery_adress varchar(255)
 );
 
