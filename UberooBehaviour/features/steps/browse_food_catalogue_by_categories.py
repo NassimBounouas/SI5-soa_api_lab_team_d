e5790a714234857785c740843688a6c4981dfa7b
Nikita Rousseau <nikita.rousseau@etu.unice.fr>
2018-11-07 15:26:28 +0100

    USE CASE 2 wip

--- UberooBehaviour/features/steps/browse_food_catalogue_by_categories.py
+++ UberooBehaviour/features/steps/browse_food_catalogue_by_categories.py
@@ -27,9 +27,33 @@ def step_impl(context):
 
 @when('listing meals by "{category}" category')
 def step_impl(context, category):
-    assert True is not False
+    japanese_exists = False
+    for c in context.categories:
+        if c['name'] == category:
+            japanese_exists = True
+    assert japanese_exists is not False
 
 
 @then('"{meal}" is available at a restaurant')
 def step_impl(context, meal):
+    meal_exists = False
+
+    meals = {}
+    formData = {
+        
+    }
+
+    # Fetch CallbackURL
+    callback_url = fetch_callback(context.endpoint, 'list_categories')
+    if len(callback_url) == 0:
+        context.failed = True
+        return
+
+    # Loop until response
+    context.categories = query_callback(callback_url, 'categories')
+    if len(context.categories) == 0:
+        context.failed = True
+        return
+
+    assert meal_exists is True
     assert context.failed is False
--- UberooBehaviour/share/fetch_callback.py
+++ UberooBehaviour/share/fetch_callback.py
@@ -4,20 +4,23 @@ import json
 from urllib import parse, request
 
 
-def fetch_callback(endpoint: str, url: str):
+def fetch_callback(endpoint: str, url: str, data: dict):
     """
     Resolve the Callback URL for an API Gateway Query
     :param endpoint: str
     :param url: str
+    :param data: dict
     :return: str
     """
     if len(endpoint) == 0:
         return ''
+    if data is None:
+        data = {}
 
     with request.urlopen(
         request.Request(
             endpoint + url
         ),
-        data=parse.urlencode({}).encode()
+        data=parse.urlencode(data).encode()
     ) as response:
         return json.loads(response.read())['callbackUrl']
