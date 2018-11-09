59ccdfdac7a82a4f67f7d920632693c177deffc9
Duminy Gaétan <gaetan.duminy@gmail.com>
2018-11-09 21:30:18 +0100

    Workflow Jamie
    Trouver les offres disponibles à l'aide sa position

--- Delivery_Service/Service/business/map_delivery.py
+++ Delivery_Service/Service/business/map_delivery.py
@@ -2,5 +2,34 @@
 # -*- coding: utf-8 -*-
 
 def map_delivery(dbh, request_id, params: dict):
-
-    return
+    """
+    Find offers around the deliverer (simulated)
+    :param dbh: database_handle
+    :param request_id: int
+    :param params: dict
+    :return: json
+    """
+    
+    longitude = int(params["long"])
+    latitude = int(params["lat"])
+    
+    with dbh.cursor() as cursor:
+        # Get list
+        sql = "SELECT * FROM to_deliver_table WHERE status = %s"
+        cursor.execute(sql, (
+            "WAITING"
+            )
+        )
+        order_list = cursor.fetchall()[0]
+        
+    
+    return {
+        'action' : 'MAP_DELIVERY_AVAILABLE_PICKUPS',
+        'message' : {
+            'status': 'OK',
+            'request': int(request_id),
+            'long': longitude,
+            'lat': latitude,
+            'orders' : order_list
+        }
+    }
