cba91c5afdb7e0b3c6bca6cfb27de350dd6f09e3
Nikita Rousseau <nikita.rousseau@etu.unice.fr>
2018-11-10 17:45:33 +0100

    Pay by credit card scenario skeleton

---
+++ UberooBehaviour/features/pay_by_credit_card.feature
@@ -0,0 +1,11 @@
+# Created by nikita at 10/11/2018
+Feature: Pay by credit card
+  As Erin,
+  I can pay directly by credit card on the platform,
+  so that I only have to retrieve my food when delivered.
+
+  @skip
+  Scenario: Pay by credit card
+    Given a client "Erin" with an order id "42"
+     When paying with my credit card number "4050841376945000", CVV "874" and expiracy "09/2025"
+     Then the order is marked as paid
---
+++ UberooBehaviour/features/steps/pay_by_credit_card.py
@@ -0,0 +1,19 @@
+from behave import *
+
+from share.fetch_callback import fetch_callback
+from share.query_callback import query_callback
+
+
+@given('a client "{client_name}" with an order id "{order_id}"')
+def step_impl(context, client_name, order_id):
+    pass
+
+
+@when('paying with my credit card number "{card_number}", CVV "{cvv}" and expiracy "{expiracy}"')
+def step_impl(context, card_number, cvv, expiracy):
+    pass
+
+
+@then('the order is marked as paid')
+def step_impl(context):
+    pass
