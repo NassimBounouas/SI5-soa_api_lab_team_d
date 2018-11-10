c347f010def402749265e435bd90dd4572c58cc8
Nikita Rousseau <nikita.rousseau@etu.unice.fr>
2018-11-10 23:56:36 +0100

    install.sh inverse test condition

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -sSf http://127.0.0.1:8080/ > /dev/null
 
-if [ $? -ne 0 ]; then
+if [ $? -eq 0 ]; then
     echo -e " \n\n${RED}### LOCALHOST PORT 8080 IS ALREADY BOUND ! PLEASE RELEASE THE PORT ! ###${NC}\n"
     exit 1
 fi
