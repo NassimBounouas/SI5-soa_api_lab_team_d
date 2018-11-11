0c95bf96288769f995628c5ecae37bd42944c780
Nassim Bounouas <nassim.bounouas@etu.unice.fr>
2018-11-11 16:57:36 +0100

    install.sh updated

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-curl -sSf http://127.0.0.1:8080/ > /dev/null
+curl -sSf http://127.0.0.1:8080/ 2> /dev/null
 
 if [ $? -eq 0 ]; then
     echo -e " \n\n${RED}### LOCALHOST PORT 8080 IS ALREADY BOUND ! PLEASE RELEASE THE PORT ! ###${NC}\n"
     exit 1
 fi
+    echo -e " \n\n${GREEN}### LOCALHOST PORT 8080 IS FREE ! WE CAN CONTINUE ! ###${NC}\n"
